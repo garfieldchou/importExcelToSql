a13c635e5e41568288f4a5f5128a4a7ffab0c5a4
garfield <cmgarfield@gmail.com>
2020-07-29 12:46:16 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace importSteamToSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
