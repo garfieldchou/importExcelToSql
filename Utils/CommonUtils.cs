d1adeab4ef36e11c40adb768729174c12a81fc59
garfield <cmgarfield@gmail.com>
2020-08-07 18:04:32 +0800

    Add a method to get week number by given date
    Copy the code from stackoverflow
    https://stackoverflow.com/a/11155102/8735281

--- Utils/CommonUtils.cs
+++ Utils/CommonUtils.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Globalization;
 using System.IO;
 using System.Data;
 using ExcelDataReader;
@@ -59,6 +61,20 @@ namespace SteamData.Utils
           return result;
         }
       }
+    }
+    public static int GetIso8601WeekOfYear(DateTime time)
+    {
+      // Seriously cheat.  If its Monday, Tuesday or Wednesday, then it'll 
+      // be the same week# as whatever Thursday, Friday or Saturday are,
+      // and we always get those right
+      DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
+      if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
+      {
+        time = time.AddDays(3);
+      }
+
+      // Return the week of our adjusted day
+      return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
     }
   }
 }
