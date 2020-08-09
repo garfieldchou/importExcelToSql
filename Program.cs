f7f0c2877b2956873994a29aa070c4b0d12c6441
garfield <cmgarfield@gmail.com>
2020-08-09 15:58:59 +0800

    Update Main method to import reports

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using static System.Console;
 using SteamData;
 using SteamData.Utils;
 using static SteamData.DownloadedStatistics.DownloadedStatisticsUtils;
+using static SteamData.GameRanks.GameRanksUtils;
+using static SteamData.HardwareSoftwareSurvey.HardwareSoftwareSurveyUtils;
 
 namespace importSteamToSql
 {
@@ -15,15 +17,26 @@ namespace importSteamToSql
 
       DateTime reportDate = DateTime.Parse("DownloadedStatistics_20200601.xlsx".Split(new[] { '_', '.' })[1].ToDateString());
 
-      var result = CommonUtils.GetExcelContent("DownloadedStatistics_20200601.xlsx");
+      // var result = CommonUtils.GetExcelContent("DownloadedStatistics_20200601.xlsx");
+      // var result = CommonUtils.GetExcelContent("Game Ranks_20200601.xlsx");
+      var result = CommonUtils.GetExcelContent("HWSSurvey_20200601.xlsx");
 
-      WriteLine(result.Tables);
-      // The result of each spreadsheet is in result.Tables
       using (var steamDb = new SteamDataContext())
       {
-        ImportCountryList(steamDb, result);
-      }
+        // DownloadedStatistics
+        // ImportCountryList(steamDb, result);
+        // ImportRegionDLStatDetail(steamDb, result);
+        // ImportCountryDLStatOverview(steamDb, result, reportDate);
+        // ImportCountryNetworkDLStat(steamDb, result, reportDate);
 
+        // GameRanks
+        // ImportOnlineStat(steamDb, result);
+        // ImportGameRanks(steamDb, result, reportDate);
+        // ImportDetailsGame(steamDb, result);
+
+        // HardwareSoftwareSurvey
+        ImportHWSurvey(steamDb, result, reportDate);
+      }
     }
   }
 }
