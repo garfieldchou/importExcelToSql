1dedfa0f5c62dbb53b97912fc053e1c6cdce858e
garfield <cmgarfield@gmail.com>
2020-08-09 15:35:49 +0800

    Add ImportHWSurvey method

---
+++ Utils/HardwareSoftwareSurvey.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Data;
+using SteamData.Utils;
+
+namespace SteamData.HardwareSoftwareSurvey
+{
+  public static class HardwareSoftwareSurveyUtils
+  {
+    public static void ImportHWSurvey(SteamDataContext db, DataSet dataSet, DateTime reportDate)
+    {
+      var hwsSurvey = dataSet.Tables[0];
+      string category = string.Empty;
+
+      for (int i = 1; i < hwsSurvey.Rows.Count; i++)
+      {
+        DataRow survey = hwsSurvey.Rows[i];
+        if (survey[1].ToString() != "") category = (string)survey[1];
+
+        db.HWSurveys.Add(new HWSurvey
+        {
+          Year = reportDate.Year,
+          Month = reportDate.Month,
+          WorkWeek = reportDate.GetIso8601WeekOfYear(),
+          Day = reportDate.Day,
+          Time = reportDate,
+          Category = category,
+          Item = survey[2].ToString(),
+          Percentage = Decimal.Parse(survey[3].ToString().Split('%')[0])
+        });
+      }
+      db.SaveChanges();
+    }
+  }
+}
