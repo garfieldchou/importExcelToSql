327b2ed82e4e75f1ba6900fdf84e028bedc7261e
garfield <cmgarfield@gmail.com>
2020-08-13 14:35:19 +0800

    Update migration files
    Add DetailsGameId foreign key to GameRanks.

--- Migrations/20200813063422_AddForeignKeyToGameRanks.Designer.cs
+++ Migrations/20200813063422_AddForeignKeyToGameRanks.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SteamData;
 
 namespace importSteamToSql.Migrations
 {
     [DbContext(typeof(SteamDataContext))]
-    partial class SteamDataContextModelSnapshot : ModelSnapshot
+    [Migration("20200813063422_AddForeignKeyToGameRanks")]
+    partial class AddForeignKeyToGameRanks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20200813063422_AddForeignKeyToGameRanks.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace importSteamToSql.Migrations
+{
+    public partial class AddForeignKeyToGameRanks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DetailsGameId",
+                table: "GameRanks",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameRanks_DetailsGameId",
+                table: "GameRanks",
+                column: "DetailsGameId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GameRanks_DetailsGames_DetailsGameId",
+                table: "GameRanks",
+                column: "DetailsGameId",
+                principalTable: "DetailsGames",
+                principalColumn: "DetailsGameId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GameRanks_DetailsGames_DetailsGameId",
+                table: "GameRanks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GameRanks_DetailsGameId",
+                table: "GameRanks");
+
+            migrationBuilder.DropColumn(
+                name: "DetailsGameId",
+                table: "GameRanks");
+        }
+    }
+}
