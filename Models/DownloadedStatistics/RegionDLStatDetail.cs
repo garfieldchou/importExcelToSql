952e9aaef6de4cc89f69a7a63aaaf3fa0571cd93
garfield <cmgarfield@gmail.com>
2020-08-13 16:16:49 +0800

    Update properties of RegionDLStatDetail

--- Models/DownloadedStatistics/RegionDLStatDetail.cs
+++ Models/DownloadedStatistics/RegionDLStatDetail.cs
@@ -9,7 +9,9 @@ namespace SteamData.DownloadedStatistics
     public int Month { get; set; }
     public int WorkWeek { get; set; }
     public int Day { get; set; }
-    public DateTime Time { get; set; }
+    public string Date { get; set; }
+    public TimeSpan Time { get; set; }
+    public DateTime Full_DateTime { get; set; }
     public string Country { get; set; }
     public decimal BandWidthGbps { get; set; }
   }
