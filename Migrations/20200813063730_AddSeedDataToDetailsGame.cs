92b70fa5f4fa19802db53650255ef461dff97807
garfield <cmgarfield@gmail.com>
2020-08-13 14:37:46 +0800

    Update migration files
    Add seed data to DetailsGames for GameRanks.

---
+++ Migrations/20200813063730_AddSeedDataToDetailsGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace importSteamToSql.Migrations
+{
+    public partial class AddSeedDataToDetailsGame : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DetailsGames",
+                columns: new[] { "DetailsGameId", "AllReviews", "Game", "GameDescription", "HotTags", "RecentReviews", "ReleaseDate", "SystemRequirements" },
+                values: new object[] { 1, "", "", "", "", "", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DetailsGames",
+                keyColumn: "DetailsGameId",
+                keyValue: 1);
+        }
+    }
+}
