070ae6491e1a45a7a5c47e2f3e24bde10a338928
garfield <cmgarfield@gmail.com>
2020-08-21 11:44:57 +0800

    Update migration files
    Remove Country column from the following tables.
    - CountryDLStatOverview
    - CountryNetworkDLStat

---
+++ Migrations/20200821050640_RemoveCountryColumn.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace importSteamToSql.Migrations
+{
+    public partial class RemoveCountryColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "CountryNetworkDLStats");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "CountryDLStatOverviews");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "CountryNetworkDLStats",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "CountryDLStatOverviews",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
