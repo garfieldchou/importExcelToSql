0ddf608369eb3571589049857218ef1566dd8d62
garfield <cmgarfield@gmail.com>
2020-08-26 12:55:46 +0800

    Update migration files

---
+++ Migrations/20200826044531_AddLinksColumnToGames.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace importSteamToSql.Migrations
+{
+    public partial class AddLinksColumnToGames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Links",
+                table: "GameRanks",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Links",
+                table: "DetailsGames",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Links",
+                table: "GameRanks");
+
+            migrationBuilder.DropColumn(
+                name: "Links",
+                table: "DetailsGames");
+        }
+    }
+}
