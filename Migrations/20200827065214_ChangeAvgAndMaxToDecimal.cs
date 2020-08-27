c280eae08ad67459a9ead28ad40552ff1d942385
garfield <cmgarfield@gmail.com>
2020-08-27 15:16:11 +0800

    Update migration files

---
+++ Migrations/20200827065214_ChangeAvgAndMaxToDecimal.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace importSteamToSql.Migrations
+{
+    public partial class ChangeAvgAndMaxToDecimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Max",
+                table: "RegionDLStatOverviews",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Average",
+                table: "RegionDLStatOverviews",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Max",
+                table: "RegionDLStatOverviews",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Average",
+                table: "RegionDLStatOverviews",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
