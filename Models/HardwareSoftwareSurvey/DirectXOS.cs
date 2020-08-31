3c7c6eec355d8d397bef01bebc6f98996ab1dbd1
garfield <cmgarfield@gmail.com>
2020-08-31 09:40:10 +0800

    Remove redundant fields in HW SW survey

--- Models/HardwareSoftwareSurvey/DirectXOS.cs
+++ Models/HardwareSoftwareSurvey/DirectXOS.cs
@@ -7,9 +7,6 @@ namespace SteamData.HardwareSoftwareSurvey
     public int DirectXOSId { get; set; }
     public int Year { get; set; }
     public int Month { get; set; }
-    public int WorkWeek { get; set; }
-    public int Day { get; set; }
-    public DateTime Time { get; set; }
     public string Category { get; set; }
     public string Item { get; set; }
     public decimal Percentage { get; set; }
--- Models/HardwareSoftwareSurvey/PcPhyCpuDetail.cs
+++ Models/HardwareSoftwareSurvey/PcPhyCpuDetail.cs
@@ -7,9 +7,6 @@ namespace SteamData.HardwareSoftwareSurvey
     public int PcPhyCpuDetailId { get; set; }
     public int Year { get; set; }
     public int Month { get; set; }
-    public int WorkWeek { get; set; }
-    public int Day { get; set; }
-    public DateTime Time { get; set; }
     public string Category { get; set; }
     public string Item { get; set; }
     public decimal Percentage { get; set; }
