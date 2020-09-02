92b92071f2a2fc1189d0c348c9b859c7d5a0787a
garfield <cmgarfield@gmail.com>
2020-09-02 14:25:07 +0800

    Use the first argument as file or directory path

--- Program.cs
+++ Program.cs
@@ -1,32 +1,54 @@
 ﻿using System;
 using System.Data;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
-using static System.IO.File;
 using static System.Console;
+using System.Linq;
+using System.Threading;
 using SteamData;
 using SteamData.Utils;
 using static SteamData.DownloadedStatistics.DownloadedStatisticsUtils;
 using static SteamData.GameRanks.GameRanksUtils;
 using static SteamData.HardwareSoftwareSurvey.HardwareSoftwareSurveyUtils;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace importSteamToSql {
+  [Command (Name = "importSteamToSql", Description = "Import Steam data from xlsx to MS SQL server")]
+  [HelpOption ("-?")]
   class Program {
-    static void Main (string[] args) {
-      if (args.Length != 1) {
-        WriteLine ("Usage: importSteamToSql.exe xxx.xlsx");
-        return;
-      }
+    static void Main (string[] args) => CommandLineApplication.Execute<Program> (args);
 
-      string fileName = args[0];
-      var fileNameChecker = new Regex (@"(DownloadedStatistics|GameRanks|HWSSurvey)_(\d{8}).xlsx$");
+    [Argument (0, Description = "xlsx file to be imported or the path containing those files")]
+    private string fileOrDirtPath { get; } = Directory.GetCurrentDirectory ();
+
+    private void OnExecute (CommandLineApplication app, CancellationToken cancellationToken = default) {
+      try {
+        ImportSteamFromDirectory (fileOrDirtPath);
+      } catch (Exception) {
+        ImportXlsxFile (fileOrDirtPath);
+      }
+    }
+
+    static void ImportSteamFromDirectory (string dirt) {
+      var files = Directory.EnumerateFiles (dirt, "*.xlsx", SearchOption.AllDirectories).OrderBy (file => file);
+      WriteLine ($"{files.Count()} files to be handled...");
+
+      foreach (var item in files) {
+        ImportXlsxFile (item);
+      }
+    }
+
+    static void ImportXlsxFile (string fileName) {
+      WriteLine ($"Handling {fileName}");
+      var fileNameChecker = new Regex (@"(DownloadedStatistics|GameRanks|HWSSurvey|Hardware_Software)_(\d{8}).xlsx$");
       Match match = fileNameChecker.Match (fileName);
 
       if (match == Match.Empty) {
         WriteLine ("File is not supported.");
         return;
       } else {
-        if (!Exists (fileName)) {
+        if (!File.Exists (fileName)) {
           WriteLine ("File does not exist.");
           return;
         } else {
