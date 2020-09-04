2d36bbc0869fa84a4706d34dc99ea036e24eb0c6
garfield <cmgarfield@gmail.com>
2020-09-04 14:13:19 +0800

    Remove using unused namespace

--- Utils/HardwareSoftwareSurvey.cs
+++ Utils/HardwareSoftwareSurvey.cs
@@ -4,7 +4,6 @@ using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Microsoft.EntityFrameworkCore;
-using SteamData.HardwareSoftwareSurvey;
 using SteamData.Utils;
 using static System.Console;
 
